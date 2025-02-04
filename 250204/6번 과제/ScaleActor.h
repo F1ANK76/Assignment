96ae4a05ee54daf92ef168b3e935a19748ec24b8
F1ANK76 <ygodlksg@gmail.com>
2025-02-04 19:41:19 +0900

    Update ScaleActor.h

--- 250204/6번 과제/ScaleActor.h
+++ 250204/6번 과제/ScaleActor.h
@@ -1,1 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ScaleActor.generated.h"
+
+UCLASS()
+class SPARTAPROJECT_API AScaleActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AScaleActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Components")
+	USceneComponent* SceneRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Item|Components")
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	float ScaleSpeed = 25;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	float MaxScaleRange = 50;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	float MinScaleRange = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	bool IsReverse = false;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
 
