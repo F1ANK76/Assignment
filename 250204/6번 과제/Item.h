49e1b2bc80a3c3ce0c5c30046c2240755836ef03
F1ANK76 <ygodlksg@gmail.com>
2025-02-04 19:41:00 +0900

    Update Item.h

--- 250204/6번 과제/Item.h
+++ 250204/6번 과제/Item.h
@@ -1,1 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h" // 리플렉션 시스템 지원, 인클루드 선언중 항상 맨 아래에 위치해야 함
+
+UCLASS() // 리플렉션 시스템에 등록
+class SPARTAPROJECT_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Components")
+	USceneComponent* SceneRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Item|Components")
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	float RotationSpeed;
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Item|Actions")
+	void ResetActorPosition();
+
+	UFUNCTION(BlueprintPure, Category="Item|Properties")
+	float GetRotationSpeed() const;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Item|Event")
+	void OnItemPicked();
+};
