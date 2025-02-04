f69543bb2a538552da1de355dcdfaca6ddb9dbe1
F1ANK76 <ygodlksg@gmail.com>
2025-02-04 19:41:11 +0900

    Update MoveActor.h

--- 250204/6번 과제/MoveActor.h
+++ 250204/6번 과제/MoveActor.h
@@ -1,1 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MoveActor.generated.h"
+
+UCLASS()
+class SPARTAPROJECT_API AMoveActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMoveActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Components")
+	USceneComponent* SceneRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Item|Components")
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	float MoveSpeed = 1500;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	float MaxMoveRange = 2000;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Properties")
+	bool IsReverse = false;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	FTimerHandle TimerHandle;  // 타이머 핸들
+
+	// 발판을 숨기고 보이게 하는 함수
+	void TogglePlatformVisibility();
+
+	bool bIsHidden = false;  // 발판이 숨겨져 있는지 여부를 추적
+};
 
