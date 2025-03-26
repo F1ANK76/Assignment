bf01d775754f0d48637af6bfbd10da316847ab6a
F1ANK76 <ygodlksg@gmail.com>
2025-03-26 19:21:31 +0900

    Create MyIDPluginBPLibrary.cpp

---
+++ 250326/MyIDPluginBPLibrary.cpp
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyIDPluginBPLibrary.h"
+#include "MyIDPlugin.h"
+
+FString UMyIDPluginBPLibrary::UserName = TEXT("");  // 정적 변수 초기화
+
+UMyIDPluginBPLibrary::UMyIDPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UMyIDPluginBPLibrary::MyIDPluginSampleFunction(float Param)
+{
+	return -1;
+}
+
+void UMyIDPluginBPLibrary::SetUserName(const FString& NewName)
+{
+    UserName = NewName;
+    UE_LOG(LogTemp, Log, TEXT("UserName set to: %s"), *UserName);
+}
+
+FString UMyIDPluginBPLibrary::GetUserName()
+{
+    return UserName;
+}
